78b336d787d84f522ea5342127bcd93659616e8b
Guoqll <529833198@qq.com>
2026-03-09 17:57:32 +0800

    ChangeNotifier

--- src/main/kotlin/com/frame/xplugins/flutter/ChangeNotifierPage.kt
+++ src/main/kotlin/com/frame/xplugins/flutter/ChangeNotifierPage.kt
@@ -1,35 +1,55 @@
 package com.frame.xplugins.flutter
 
-import java.util.*
 import com.frame.plugin.base.BaseAnActionForFlutter
 
-class ChangeNotifierPage : BaseAnActionForFlutter("ChangeNotifier Page") {
+class ChangeNotifierPage : BaseAnActionForFlutter("ChangeNotifier") {
 
     private val codePath = "/code/flutter/change_notifier"
+
+    // 扩展函数：将驼峰式命名转换为蛇形命名（首字母小写，大写字母前加下划线）
+    private fun String.toSnakeCase(): String {
+        if (isEmpty()) return this
+        val result = StringBuilder()
+        // 首字母直接转小写
+        result.append(this[0].lowercaseChar())
+        for (i in 1 until length) {
+            val currentChar = this[i]
+            if (currentChar.isUpperCase()) {
+                // 大写字母前加下划线并转小写
+                result.append('_').append(currentChar.lowercaseChar())
+            } else {
+                result.append(currentChar)
+            }
+        }
+        return result.toString()
+    }
+
+    // 扩展函数：将字符串的首字母转为小写
+    fun String.toLowerCaseFirstChar(): String {
+        if (this.isEmpty()) return this
+        return this[0].lowercaseChar() + this.substring(1)
+    }
+// 示例: NewMain → new_main
 
     override fun generationXcode(
         filePath: String, mainPath: String, packageName: String, className: String, resLimit: String
     ) {
-        /*var resLimitStr = ""
-        var resLimitStr_ = ""
-        if (resLimit.isNotEmpty()) {
-            resLimitStr = resLimit.lowercase(Locale.getDefault())
-            resLimitStr_ = "${resLimit.lowercase(Locale.getDefault())}_"
-        }*/
+        //1、Page
         writeToFile(
             filepath = filePath,
-            filename = "${className.lowercase(Locale.getDefault())}_page.dart",
+            filename = "${className.toSnakeCase()}_page.dart",
             content = readFile("$codePath/simple_page.dart")!!
-                .replace("^Simple^", className),
+                .replace("Simple", className)
+                .replace("simple", className.toSnakeCase()),
         )
-
+        //2、viewModel
         writeToFile(
             filepath = filePath,
-            filename = "${className.lowercase(Locale.getDefault())}_state.dart",
-            content = readFile("$codePath/simple_notifier.dart")!!
-                .replace("^Simple^", className),
+            filename = "${className.toSnakeCase()}_view_model.dart",
+            content = readFile("$codePath/simple_view_model.dart")!!
+                .replace("Simple", className)
+                .replace("simple", className.toSnakeCase()),
         )
-
     }
 
 
